778e2f2ab5b4d2070a9c49ef184cc0e395dc1de8
yuanbowen <r03944049@ntu.edu.tw>
2019-06-30 16:18:26 +0800

    -

--- Gauss/example.m
+++ Gauss/example.m
@@ -2,23 +2,31 @@
 make;
 
 % set model parameters
-lambda_w = 0.0625; lambda_U = 0.0625; lambda_V = 0.0625; d = 4;
+lambda_U = 128; lambda_V = 128; d = 40;
 
 % set training algorithm's parameters
-epsilon = 0.01; do_pcond = true; sub_rate = 0.1;
+epsilon = 0.01; do_pcond = false;
 
 % prepare training and test data sets
-[y,X] = libsvmread('fourclass_scale.tr');
-[y_test,X_test] = libsvmread('fourclass_scale.te');
-n = max(size(X,2),size(X_test,2));
-[i,j,s] = find(X);
-X = sparse(i,j,s,size(X,1),n);
-[i,j,s] = find(X_test);
-X_test = sparse(i,j,s,size(X_test,1),n);
+[y, W, H] = libsvmread('./ml/ml.tr');
+[y_test,W_test, H_test] = libsvmread('./ml/ml.te');
+
+
+n = max(size(W,2),size(W_test,2));
+[i,j,s] = find(W);
+W = sparse(i,j,s,size(W,1),n);
+[i,j,s] = find(W_test);
+W_test = sparse(i,j,s,size(W_test,1),n);
+
+n = max(size(H,2),size(H_test,2));
+[i,j,s] = find(H);
+H = sparse(i,j,s,size(H,1),n);
+[i,j,s] = find(H_test);
+H_test = sparse(i,j,s,size(H_test,1),n);
 
 % learn an FM model
-[w, U, V] = fm_train(y, X, lambda_w, lambda_U, lambda_V, d, epsilon, do_pcond, sub_rate);
+[w, U, V] = fm_train(y, W, H, lambda_U, lambda_V, d, epsilon, do_pcond);
 
 % do prediction
-y_tilde = fm_predict(X_test, w, U, V);
-display(sprintf('test accuracy: %f', sum(sign(y_tilde) == y_test)/size(y_test,1)));
+%y_tilde = fm_predict(X_test, w, U, V);
+%display(sprintf('test accuracy: %f', sum(sign(y_tilde) == y_test)/size(y_test,1)));

1186e836d04ebdaa9cf8719109abe6fb6852abce
johncreed <hsiajuiyang5174@gmail.com>
2019-07-09 23:55:07 +0800

    Modify script

--- script/ant-vs-gauss.sh
+++ script/ant-vs-gauss.sh
@@ -1,8 +1,10 @@
 d=40
-tr='ml.tr'
-va='ml.te'
+#tr='ml.tr'
+#va='ml.te'
 #tr='ml.tr.10w'
 #va='ml.tr.10w'
+tr='nf.tr'
+va='nf.te'
 
 log_path="logs/$tr.$va.$d"
 mkdir -p $log_path
@@ -14,13 +16,13 @@ log_name="$tr.$va.$d.$lambda_U.$lambda_V"
 matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';run('example.m');exit;" > $log_path/$log_name& 
 
 
-lambda_U=0.5
-lambda_V=0.5
-log_name="$tr.$va.$d.$lambda_U.$lambda_V"
-matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';run('example.m');exit;" > $log_path/$log_name& 
+#lambda_U=0.5
+#lambda_V=0.5
+#log_name="$tr.$va.$d.$lambda_U.$lambda_V"
+#matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';run('example.m');exit;" > $log_path/$log_name& 
 
 
-lambda_U=0.005
-lambda_V=0.005
-log_name="$tr.$va.$d.$lambda_U.$lambda_V"
-matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';run('example.m');exit;" > $log_path/$log_name& 
+#lambda_U=0.005
+#lambda_V=0.005
+#log_name="$tr.$va.$d.$lambda_U.$lambda_V"
+#matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';run('example.m');exit;" > $log_path/$log_name& 

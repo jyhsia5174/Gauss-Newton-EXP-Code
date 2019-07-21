5a30e62164ab40b735a1d434d9b12d7a86f63749
johncreed <hsiajuiyang5174@gmail.com>
2019-07-21 18:35:53 +0800

    update run script

--- script/ant-vs-gauss.sh
+++ script/ant-vs-gauss.sh
@@ -10,11 +10,11 @@
 #tr='nf.tr'
 #va='nf.te'
 
-d=4
+d=40
 tr='ratings.dat.trva'
 va='ratings.dat.te'
 
-max_iter=100
+max_iter=2000
 
 log_path="logs/$tr.$va.$d"
 mkdir -p $log_path
@@ -26,13 +26,13 @@ log_name="$tr.$va.$d.$lambda_U.$lambda_V"
 matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';max_iter=${max_iter};run('example.m');exit;" > $log_path/$log_name& 
 
 
-#lambda_U=0.5
-#lambda_V=0.5
-#log_name="$tr.$va.$d.$lambda_U.$lambda_V"
-#matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';max_iter=${max_iter};run('example.m');exit;" > $log_path/$log_name& 
+lambda_U=0.5
+lambda_V=0.5
+log_name="$tr.$va.$d.$lambda_U.$lambda_V"
+matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';max_iter=${max_iter};run('example.m');exit;" > $log_path/$log_name& 
 
 
-#lambda_U=0.005
-#lambda_V=0.005
-#log_name="$tr.$va.$d.$lambda_U.$lambda_V"
-#matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';max_iter=${max_iter};run('example.m');exit;" > $log_path/$log_name& 
+lambda_U=0.005
+lambda_V=0.005
+log_name="$tr.$va.$d.$lambda_U.$lambda_V"
+matlab -nodisplay -nosplash -nodesktop -r "lambda_U=${lambda_U};lambda_V=${lambda_V};d=${d};tr='${tr}';va='${va}';max_iter=${max_iter};run('example.m');exit;" > $log_path/$log_name& 

84db2e5c834b2493a45de307203c24adeb79b6fd
Yu-Ting <r05922038@ntu.edu.tw>
2020-01-29 16:08:36 +0800

    comment 319-330

--- MF/ANT/fm_train.m
+++ MF/ANT/fm_train.m
@@ -10,41 +10,38 @@ function [U, V] = fm_train(R, U, V, U_reg, V_reg, epsilon, max_iter, R_test)
 %   R_test: testing rating matrix
 % Outputs:
 %   U, V: the interaction (d-by-n) matrices.
-    tic;
    
     [m, n] = size(R);
     nnz_R_test = nnz(R_test);
 
-    fprintf('%4s  %15s  %3s  %15s  %15s  %15s  %15s  %15s\n', 'iter', 'time', '#cg', 'obj', '|G_U|', 'test_loss', '|G|', 'loss');
+    fprintf('%4s  %15s  %3s  %15s  %15s  %15s  %15s  %15s  %15s\n', 'iter', 'time', '#cg', 'obj', '|G_U|', '|G_V|', 'test_loss', '|G|', 'loss');
     for k = 1:max_iter
         if (k == 1)
             B = get_embedding_inner(U, V, R)-R;
             loss = 0.5 * full(sum(sum(B .* B)));
             f = 0.5*(sum(U.*U)*U_reg+sum(V.*V)*V_reg)+loss;
-            GU = U*spdiags(U_reg,0,m,m)+V*B';
-            GV = V*spdiags(V_reg,0,n,n)+U*B;
-            G_norm = norm([GU GV]);
+            GU = U.*U_reg'+V*B';
+            GV = V.*V_reg'+U*B;
+            G_norm = norm([GU GV],'fro');
             G_norm_0 = G_norm;
             fprintf('initial G_norm: %15.6f\n', G_norm_0);
         end
 
-        [U, B, f, loss, G_norm_U, cg_iters_U] = update_block(U, V, B, R, GU, f, loss, U_reg);
+        time1=tic;
+        GU = U.*U_reg'+V*B';
+        [U, B, f, loss, cg_iters_U] = update_block(U, V, B, R, GU, f, loss, U_reg, 'no_transposed');
+        GV = V.*V_reg'+U*B;
+        [V, B, f, loss, cg_iters_V] = update_block(V, U, B, R, GV, f, loss, V_reg, 'transposed');
+        time2=toc(time1);
 
         Y_test_tilde = get_embedding_inner(U,V,R_test);
         test_loss = sqrt(full(sum(sum((R_test-Y_test_tilde).*(R_test-Y_test_tilde)))/nnz_R_test));
-        GU = U*spdiags(U_reg,0,m,m)+V*B';
-        GV = V*spdiags(V_reg,0,n,n)+U*B;
-        G_norm = norm([GU GV]);
-        fprintf('%4d  %15.3f  %3d  %15.3f  %15.6f  %15.6f  %15.6f  %15.3f\n', k, toc, cg_iters_U, f, G_norm_U, test_loss, G_norm, loss);
-
-        [V, B, f, loss, G_norm_V, cg_iters_V] = update_block(V, U, B', R', GV, f, loss, V_reg);
-        B = B';
-        Y_test_tilde = get_embedding_inner(U,V,R_test);
-        test_loss = sqrt(full(sum(sum((R_test-Y_test_tilde).*(R_test-Y_test_tilde)))/nnz_R_test));
-        GU = U*spdiags(U_reg,0,m,m)+V*B';
-        GV = V*spdiags(V_reg,0,n,n)+U*B;
-        G_norm = norm([GU GV]);
-        fprintf('%4d  %15.3f  %3d  %15.3f  %15.6f  %15.6f  %15.6f  %15.3f\n', k, toc, cg_iters_V, f, G_norm_V, test_loss, G_norm, loss);
+        GU = U.*U_reg'+V*B';
+        GV = V.*V_reg'+U*B;
+        G_norm = norm([GU GV],'fro');
+        G_norm_U = norm(GU,'fro');
+        G_norm_V = norm(GV,'fro');
+        fprintf('%4d  %15.3f  %3d  %15.3f  %15.6f  %15.6f  %15.6f  %15.6f  %15.3f\n', k, time2, cg_iters_V, f, G_norm_U, G_norm_V, test_loss, G_norm, loss);
 
         if (G_norm <= epsilon*G_norm_0)
             break;
@@ -54,12 +51,9 @@ function [U, V] = fm_train(R, U, V, U_reg, V_reg, epsilon, max_iter, R_test)
         fprintf('Warning: reach max training iteration. Terminate training process.\n');
     end
 end
-% See Algorithm 3 in the paper.function [U, B, f, loss, G_norm, cg_iters] = update_block(U, V, B, R, G, f, loss, reg)
-function [U, B, f, loss, G_norm, cg_iters] = update_block(U, V, B, R, G, f, loss, reg)
+function [U, B, f, loss, cg_iters] = update_block(U, V, B, R, G, f, loss, reg, option)
     eta = 0.3;
     cg_max_iter = 20;
-    [m, n] = size(R);
-    G_norm = norm(G,'fro');
     Su = zeros(size(G));
     C = -G;
     D = C;
@@ -68,8 +62,13 @@ function [U, B, f, loss, G_norm, cg_iters] = update_block(U, V, B, R, G, f, loss
     cg_iters = 0;
     while (gamma > eta*eta*gamma_0)
         cg_iters = cg_iters+1;
-        Z = get_embedding_inner(D, V, R);
-        Dh = D*spdiags(reg,0,m,m)+V*Z';
+        if strcmp(option,'transposed')
+            Z = get_embedding_inner(V, D, R);
+            Dh = D.*reg'+V*Z;
+        else
+            Z = get_embedding_inner(D, V, R);
+            Dh = D.*reg'+V*Z';
+        end
         alpha = gamma/sum(sum(D.*Dh));
         Su = Su+alpha*D;
         C = C-alpha*Dh;
@@ -82,8 +81,11 @@ function [U, B, f, loss, G_norm, cg_iters] = update_block(U, V, B, R, G, f, loss
             break;
         end
     end
-
-    Delta = get_embedding_inner(Su, V, R);
+    if strcmp(option,'transposed')
+        Delta = get_embedding_inner(V, Su, R);
+    else
+        Delta = get_embedding_inner(Su, V, R);
+    end
     B_new = B+Delta;
     USu = sum(U.*Su);
     SuSu = sum(Su.*Su);
@@ -96,7 +98,7 @@ function [U, B, f, loss, G_norm, cg_iters] = update_block(U, V, B, R, G, f, loss
 end
 %point wise summation
 % z_(m,n) = u_m^T*v_n
-function [Z] = get_embedding_inner(U, V, R)
+function Z = get_embedding_inner(U, V, R)
     [m, n] = size(R);
     [i_idx, j_idx, vals] = find(R);
     l = nnz(R);

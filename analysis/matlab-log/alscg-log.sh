bc6d9fe64991dd1b7db81190df66d0e1916ae974
jyhsia <hsiajuiyang5174@gmail.com>
2021-07-12 17:19:21 +0800

    Add Draw figure7 script.

---
+++ analysis/matlab-log/alscg-log.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+solver=1
+enable_gpu=0
+lambda=(1 5e-1 1e-1 5e-2 1e-2 5e-3 1e-3)
+d=40
+t=100
+eta=0.3
+cgt=20
+
+log_dir='log'
+task(){
+  for l2 in ${lambda[@]}; do
+      log="sol_${solver}_gpu_${enable_gpu}_l2_${l2}_d_${d}_t_${t}_eta_${eta}_cgt_${cgt}"
+      awk '{ if(NF == 10) { printf("%s", $1); for(i = 2; i <= NF; i++) printf(",%s" ,$i); printf("\n");  } }' ${log_dir}/${log} > ${log}.csv 
+  done
+}
+
+task

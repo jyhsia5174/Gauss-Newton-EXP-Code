ce6e95027e14c20ba73c41f674770f556213bde6
jyhsia <hsiajuiyang5174@gmail.com>
2021-07-14 15:25:23 +0800

    Add fig1, 2, 7 ipynb Combine alscg-log.sh als.sh gauss-log.sh to to_csv.sh Track log and csv

---
+++ analysis/matlab-log/to_csv.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+log_dir='log'
+task(){
+  for log in ${log_dir}/*
+  do
+      awk '{ if(NF == 10) { printf("%s", $1); for(i = 2; i <= NF; i++) printf(",%s" ,$i); printf("\n");  } }' ${log} > ${log##*/}.csv 
+  done
+}
+
+task

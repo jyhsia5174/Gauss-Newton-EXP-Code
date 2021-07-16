c9296966f753faa4c35d9ba3bd47eafdfd4583d8
johncreed <hsiajuiyang5174@gmail.com>
2021-07-16 12:12:30 +0800

    Add time run script

---
+++ libmf_matlab/gauss-time.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#function run(solver, enable_gpu, l2, d, t, eta, cgt)
+#  % function run(solver, enable_gpu, l2, d, t)
+#  % Inputs:
+#  % solver: 
+#  %   - 0: gauss
+#  %   - 1: alscg
+#  %   - 2: alscg
+#  % enable_gpu: 
+#  %   - 0: disable
+#  %   - 1: enable
+#  % l2: regularization
+#  % d: embedding dimemsion
+#  % t: iteration
+#  % eta: cg tightness
+#  % cgt: max cg iteration
+
+# Parse arg
+dry_run=false
+enable_gpu=false
+num_proc=1
+while getopts 'nhgc:' o; do
+    case ${o} in
+        n) 
+          dry_run=true
+          ;;
+        c)
+          num_proc=${OPTARG}
+          ;;
+        g)
+          echo 'Enable gpu'
+          enable_gpu=true
+          ;;
+        h | *) 
+          echo './gauss.sh [-n] [-g] [-c <num>]' >&2
+          exit 1
+    esac
+done
+
+solver=0
+if ${enable_gpu}
+then
+  enable_gpu=1
+else
+  enable_gpu=0
+fi
+lambda=(5e-1 5e-2 5e-3)
+d=40
+t=100
+eta=0.3
+cgt=20
+
+log_dir="log"
+mkdir -p ${log_dir}
+task(){
+  for l2 in ${lambda[@]}; do
+      log="sol_${solver}_gpu_${enable_gpu}_l2_${l2}_d_${d}_t_${t}_eta_${eta}_cgt_${cgt}"
+      echo "exec su matlab -c 'matlab -nodisplay -nosplash -nodesktop -r \"run(${solver}, ${enable_gpu}, ${l2}, ${d}, ${t}, ${eta}, ${cgt}); exit;\" > ${log_dir}/${log} '"
+  done
+}
+
+if ${dry_run}; then
+  echo "Dry run -c ${num_proc}"
+  task
+else
+  echo "Run"
+  task | xargs -0 -d '\n' -P ${num_proc} -I {} sh -c "{}" &
+fi
+
+wait
---
+++ perm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+(
+cd /tmp2/johncreed/Gauss-Newton-EXP-Code/
+pwd
+chmod a=u -R *
+)

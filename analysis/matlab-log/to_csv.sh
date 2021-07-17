1b4cd57e1483de6a92b5b3a3401ae5c90eca8368
jyhsia <hsiajuiyang5174@gmail.com>
2021-07-17 13:31:17 +0800

    update csv

--- analysis/matlab-log/to_csv.sh
+++ analysis/matlab-log/to_csv.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 log_dir='log'
 task(){
-  for log in ${log_dir}/*
+  for log in ${log_dir}/sol*
   do
       awk '{ if(NF == 10) { printf("%s", $1); for(i = 2; i <= NF; i++) printf(",%s" ,$i); printf("\n");  } }' ${log} > ${log##*/}.csv 
   done
